922c6f37df9fe3ba1c20e2f8d9087fb1b21f5637
Mikenunz1 <mikenunzioo@gmail.com>
2025-01-26 21:08:50 -0800

    Comments for player movement demo
    Added some small comments to the move demo to explain the code a bit more.

--- Game_Files/Scripts/player_demo.gd
+++ Game_Files/Scripts/player_demo.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 350.0
 
 func _physics_process(_delta):	
 	
+	#Input_directon is a vector variable determined by user input. 
 	var input_direction = Input.get_vector("Move_Left", "Move_Right", "Move_Up", "Move_Down")
+	
 	velocity = input_direction * SPEED
 
+	#Built in function within Godot that deals with player movement
 	move_and_slide()
